49499b7b8736c95716be2c56543d5d2f0a0e48ce
domfindlay5 <p16170444@my365.dmu.ac.uk>
2019-04-01 20:37:26 +0100

    Added item variables and get text feature

--- app/src/main/java/com/example/dom/trustedreviews/ItemsActivity.java
+++ app/src/main/java/com/example/dom/trustedreviews/ItemsActivity.java
@@ -5,18 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ItemsActivity extends AppCompatActivity {
 
     ProgressBar prg;
+    String item1,item2,item3,item4;
+
+    EditText text_input_item1;
+    EditText text_input_item2;
+    EditText text_input_item3;
+    EditText text_input_item4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
-
 
         TextView stepsTxtView = (TextView) findViewById(R.id.stepstxtView);
         stepsTxtView.setText("Step 1 of 4:");
@@ -24,17 +31,35 @@ public class ItemsActivity extends AppCompatActivity {
         prg=(ProgressBar)findViewById(R.id.progressBar);
         prg.setProgress(25);
 
+
+        text_input_item1 = (EditText) findViewById(R.id.text_input_item1);
+        text_input_item2 = (EditText) findViewById(R.id.text_input_item2);
+        text_input_item3 = (EditText) findViewById(R.id.text_input_item3);
+        text_input_item4 = (EditText) findViewById(R.id.text_input_item4);
+
         // Next Button to Ratings
         // set object
         Button nextPgBtn = (Button)findViewById(R.id.nextBtn);
         // pass on click listener
-        nextPgBtn.setOnClickListener(new View.OnClickListener() {
+        nextPgBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent startIntent = new Intent(getApplicationContext(),RatingActivity.class);
                 startActivity(startIntent);
+
+                item1 = text_input_item1.getText().toString();
+                item2 = text_input_item2.getText().toString();
+                item3 = text_input_item3.getText().toString();
+                item4 = text_input_item4.getText().toString();
+
+                showToast(item1 + item2 + item3 + item4);
             }
         });
 
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(ItemsActivity.this,text, Toast.LENGTH_SHORT).show();
     }
 }
