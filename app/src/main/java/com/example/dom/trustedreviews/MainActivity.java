85a9a25f0511e890b710bcd19b0ff030f7461de2
domfindlay5 <p16170444@my365.dmu.ac.uk>
2019-04-01 20:38:36 +0100

    Added debug message OnStart, onResume and onStop

--- app/src/main/java/com/example/dom/trustedreviews/MainActivity.java
+++ app/src/main/java/com/example/dom/trustedreviews/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.dom.trustedreviews;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    String msg = "Android :";
+
+
+    /* called when the activity is created */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    /* called when the activity is about to become visible to the user */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg,"OnStart() event");
+    }
+
+
+    /* called when the activity is visible */
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(msg, "On Resume() event");
+    }
+
+
+    /* called when the activity is not visible*/
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(msg,"On stop () event");
     }
 
 }
