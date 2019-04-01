641e30c3d808bebf21cab3aa4992c1ae72a7e258
domfindlay5 <p16170444@my365.dmu.ac.uk>
2019-04-01 20:44:31 +0100

    Added camera and external storage permissions

--- app/src/main/java/com/example/dom/trustedreviews/RatingActivity.java
+++ app/src/main/java/com/example/dom/trustedreviews/RatingActivity.java
@@ -5,39 +5,68 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RatingActivity extends AppCompatActivity {
 
     ProgressBar prg;
     RatingBar ratingBar;
+    Float rating1,rating2,rating3,rating4;
+
+    RatingBar ratingBar1;
+    RatingBar ratingBar2;
+    RatingBar ratingBar3;
+    RatingBar ratingBar4;
+
+
     @Override
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
 
         TextView stepsTxtView = (TextView) findViewById(R.id.stepstxtView);
         stepsTxtView.setText("Step 2 of 4:");
 
-        prg=(ProgressBar)findViewById(R.id.progressBar);
+        prg = (ProgressBar) findViewById(R.id.progressBar);
         prg.setProgress(50);
 
-        ratingBar = (RatingBar) findViewById(R.id.ratingBar1);
+        ratingBar1 = (RatingBar) findViewById(R.id.ratingBar1);
+        ratingBar2 = (RatingBar) findViewById(R.id.ratingBar2);
+        ratingBar3 = (RatingBar) findViewById(R.id.ratingBar3);
+        ratingBar4 = (RatingBar) findViewById(R.id.ratingBar4);
 
 
         // next button to comments
-        Button nextPgBtn = (Button)findViewById(R.id.nextBtn);
+        Button nextPgBtn = (Button) findViewById(R.id.nextBtn);
         // pass on click listener
         nextPgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(),CommentsActivity.class);
+
+                Intent startIntent = new Intent(getApplicationContext(), CommentsActivity.class);
                 startActivity(startIntent);
+
+                /*rating1 = ratingBar1.getRating();
+                rating2 = ratingBar2.getRating();
+                rating3 = ratingBar3.getRating();
+                rating4 = ratingBar4.getRating();
+
+                showToast(rating1);
+                showToast(rating2);
+                showToast(rating3);
+                showToast(rating4); */
+
+
             }
         });
 
     }
+        private void showToast(float msg) {
+            Toast.makeText(RatingActivity.this, (int) msg, Toast.LENGTH_SHORT).show();
+        }
 
-}
+    }
