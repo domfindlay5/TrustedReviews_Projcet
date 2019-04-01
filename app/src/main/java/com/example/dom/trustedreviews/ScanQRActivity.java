5ce7dec8dbf6d962203a0490869632c97cd24ab3
domfindlay5 <p16170444@my365.dmu.ac.uk>
2019-04-01 20:39:10 +0100

    Removed ZxingScanner due to compatability issues

--- app/src/main/java/com/example/dom/trustedreviews/ScanQRActivity.java
+++ app/src/main/java/com/example/dom/trustedreviews/ScanQRActivity.java
@@ -1,49 +1,30 @@
 package com.example.dom.trustedreviews;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
-import com.google.zxing.Result;
+public class ScanQRActivity extends AppCompatActivity {
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
-public class ScanQRActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
-    private ZXingScannerView ScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qr);
-    }
 
-    public void scan(View view){
-        ScannerView = new ZXingScannerView(getApplicationContext());
-        setContentView(ScannerView);
-        ScannerView.setResultHandler(this);
-        ScannerView.startCamera();
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        ScannerView.stopCamera();
-    }
-
-    @Override
-    public void handleResult(Result result){
-        String scanResult = result.getText();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        // Next Button to Preview
+        // set object
+        Button nextPgBtn = (Button)findViewById(R.id.nextBtn);
+        // pass on click listener
+        nextPgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                ScannerView.resumeCameraPreview(ScanQRActivity.this);
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),ItemsActivity.class);
+                startActivity(startIntent);
             }
         });
-    }
 
+    }
 }
